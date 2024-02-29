ec6e7151bf0bd37b7d7e0313a8adf44467b6a542
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-02-29 17:34:24 -0300

    Add UnityPackage

---
+++ LuaEngine.Editor/Assets/LuaEngine/Mono/ScriptBehavior.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LuaEngine.Mono
+{
+    public class ScriptBehavior : MonoBehaviour
+    {
+        public string LuaScriptName = null;
+    }
+}
