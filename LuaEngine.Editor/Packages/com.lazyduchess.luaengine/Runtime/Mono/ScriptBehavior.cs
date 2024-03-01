3764cf46b9e448792e5220b34088bbcc0f04e5b2
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-03-01 00:16:20 -0300

    add script values to package

--- LuaEngine.Editor/Packages/com.lazyduchess.luaengine/Runtime/Mono/ScriptBehavior.cs
+++ LuaEngine.Editor/Packages/com.lazyduchess.luaengine/Runtime/Mono/ScriptBehavior.cs
@@ -6,6 +6,7 @@ namespace LuaEngine.Mono
 {
     public class ScriptBehavior : MonoBehaviour
     {
+        [Header("Filename of a Lua Behavior Script (e.g. \"doctorpolo.triggerhurt.lua\")")]
         public string LuaScriptName = null;
     }
 }
---
+++ LuaEngine.Editor/Packages/com.lazyduchess.luaengine/Runtime/Mono/ScriptValue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace LuaEngine.Mono
+{
+    public abstract class ScriptValue : MonoBehaviour
+    {
+        public string Name = "";
+    }
+}
