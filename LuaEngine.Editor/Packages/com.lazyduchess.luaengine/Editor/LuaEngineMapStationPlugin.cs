57e7a0b8e8fe06572caba94fde65be5044668184
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-03-01 11:27:21 -0300

    Delete lua meta files

--- LuaEngine.Editor/Packages/com.lazyduchess.luaengine/Editor/LuaEngineMapStationPlugin.cs
+++ LuaEngine.Editor/Packages/com.lazyduchess.luaengine/Editor/LuaEngineMapStationPlugin.cs
@@ -18,6 +18,11 @@ namespace LuaEngine.Editor
         private string MakeLuaZip(string assetPath, string luaPath)
         {
             var luaOutputPath = Path.Combine(assetPath, "lua.luamod");
+            var metaFiles = Directory.GetFiles(luaPath, "*.meta");
+            foreach(var metaFile in metaFiles)
+            {
+                File.Delete(metaFile);
+            }
             ZipFile.CreateFromDirectory(luaPath, luaOutputPath);
             return luaOutputPath;
         }
