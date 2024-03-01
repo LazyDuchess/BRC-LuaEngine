05d8c7d3f5acb86c1853656b0e1e193d1704e447
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-02-29 22:38:39 -0300

    Add LuaAudioSource and MIxerGroups enum

---
+++ LuaEngine/LuaAudioSource.cs
@@ -0,0 +1,43 @@
+﻿using MoonSharp.Interpreter;
+using Reptile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace LuaEngine
+{
+    [MoonSharpUserData]
+    public class LuaAudioSource : LuaBuiltInBehaviour
+    {
+        private AudioSource _handle = null;
+
+        [MoonSharpHidden]
+        public LuaAudioSource(AudioSource audio, Script script) : base(audio, script)
+        {
+            _handle = audio;
+        }
+
+        public void Play()
+        {
+            _handle.Play();
+        }
+
+        public void Stop()
+        {
+            _handle.Stop();
+        }
+
+        public void Pause()
+        {
+            _handle.Pause();
+        }
+
+        public void SetMixerGroup(int mixerGroup)
+        {
+            _handle.outputAudioMixerGroup = Core.Instance.AudioManager.mixerGroups[mixerGroup];
+        }
+    }
+}
---
+++ lua/autorun/luaengine.MixerGroups.lua
@@ -0,0 +1,11 @@
+--PRIORITY: -100
+
+MixerGroups = {}
+
+MixerGroups.Master = 0
+MixerGroups.Music = 4
+MixerGroups.SFX = 1
+MixerGroups.UI = 2
+MixerGroups.Gameplay = 3
+MixerGroups.Voices = 5
+MixerGroups.Ambience = 6
