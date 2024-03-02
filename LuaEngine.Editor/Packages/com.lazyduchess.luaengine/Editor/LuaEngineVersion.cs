82f108d2198f105f59112cfbaf6270731480c983
LazyDuchess <lattechariot@gmail.com>
2024-03-02 12:17:43 -0300

    v0.2.1

--- LuaEngine.Editor/Packages/com.lazyduchess.luaengine/Editor/LuaEngineVersion.cs
+++ LuaEngine.Editor/Packages/com.lazyduchess.luaengine/Editor/LuaEngineVersion.cs
@@ -2,7 +2,7 @@ namespace LuaEngine.Editor
 {
     public static class LuaEngineVersion
     {
-        public const string Version = "0.2.0";
+        public const string Version = "0.2.1";
     }
 }
 
