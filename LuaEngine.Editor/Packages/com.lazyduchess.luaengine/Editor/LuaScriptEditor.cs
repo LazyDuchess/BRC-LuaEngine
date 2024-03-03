12186c4a1dbcfe4fcf6e391f6a6f2218c6a492b1
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-03-03 12:04:01 -0300

    Documentation, create lua folder

--- LuaEngine.Editor/Packages/com.lazyduchess.luaengine/Editor/LuaScriptEditor.cs
+++ LuaEngine.Editor/Packages/com.lazyduchess.luaengine/Editor/LuaScriptEditor.cs
@@ -2,6 +2,7 @@
 using UnityEditor;
 using System.IO;
 using UnityEditor.VersionControl;
+using static UnityEditor.FilePathAttribute;
 
 namespace LuaEngine.Editor
 {
@@ -27,12 +28,27 @@ end
 script.OnStart.Add(Start)
 updateCallback = Core.OnUpdate.Add(Update)
 script.OnDestroy.Add(OnDestroy)";
-        private const string AutorunScriptContents = @"--PRIORITY: 0
+        private const string AutorunScriptContents = @"--Priority comment can be skipped. Scripts with lower priority load first.
+--PRIORITY: 0
 
+-- Global variables can be read and written from any script.
 MyMathLibrary = {}
 function MyMathLibrary.Add(x, y)
     return x + y
 end";
+        [MenuItem("Assets/Create/Lua/Lua Folder", false, -100)]
+        public static void CreateLuaFolder()
+        {
+            var path = GetCurrentFolder();
+            var luaFolderPath = Path.Combine(GetCurrentFolder(), "lua");
+            var autorunFolderPath = Path.Combine(luaFolderPath, "autorun");
+            var behaviorFolderPath = Path.Combine(luaFolderPath, "behavior");
+            Directory.CreateDirectory(luaFolderPath);
+            Directory.CreateDirectory(autorunFolderPath);
+            Directory.CreateDirectory(behaviorFolderPath);
+            AssetDatabase.Refresh();
+            Selection.activeObject = AssetDatabase.LoadAssetAtPath<DefaultAsset>(luaFolderPath);
+        }
 
         [MenuItem("Assets/Create/Lua/Empty Script", false)]
         public static void CreateEmptyLuaScript()
@@ -52,17 +68,22 @@ end";
             CreateScript("New Autorun Script", AutorunScriptContents);
         }
 
-        private static void CreateScript(string name, string contents)
+        private static string GetCurrentFolder()
         {
             var location = "Assets";
             var activeObject = Selection.activeObject;
             if (activeObject != null)
             {
                 location = AssetDatabase.GetAssetPath(activeObject);
-                Debug.Log(activeObject.GetType());
             }
             if (File.Exists(location))
                 location = Path.GetDirectoryName(location);
+            return location;
+        }
+
+        private static void CreateScript(string name, string contents)
+        {
+            var location = GetCurrentFolder();
             location = GetUniqueFileName(Path.Combine(location, name), ".lua");
             File.WriteAllText(location, contents);
             AssetDatabase.Refresh();
