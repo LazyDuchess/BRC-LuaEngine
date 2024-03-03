aeed297507b98c7fea78d1b7fdfc244642f05e03
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-03-03 09:41:19 -0300

    Vector3: Add a lot more math

--- lua/autorun/luaengine.Vector3.lua
+++ lua/autorun/luaengine.Vector3.lua
@@ -18,11 +18,44 @@ function Vector3.Multiply(vec, multiplier)
 	return vec;
 end
 
+function Vector3.Divide(vec, divisor)
+	vec.x = vec.x / divisor
+	vec.y = vec.y / divisor
+	vec.z = vec.z / divisor
+end
+
 function Vector3.Add(v1, v2)
 	v1.x = v1.x + v2.x
 	v1.y = v1.y + v2.y
 	v1.z = v1.z + v2.z
 	return v1;
+end
+
+function Vector3.Subtract(v1, v2)
+	v1.x = v1.x - v2.x
+	v1.y = v1.y - v2.y
+	v1.z = v1.z - v2.z
+	return v1;
+end
+
+function Vector3.Length(vec)
+	return math.sqrt(vec.x*vec.x+vec.y*vec.y+vec.z*vec.z)
+end
+
+function Vector3.Normalize(vec)
+	local length = Vector3.Length(vec)
+	if length ~= 0 then
+		return Vector3.Divide(vec, length)
+	end
+	return vec
+end
+
+function Vector3.Distance(v1, v2)
+	return Vector3.Length(Vector3.Subtract(v1, v2))
+end
+
+function Vector3.Dot(v1, v2)
+	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
 end
 
 Vector3.Up = Vector3.New(0, 1, 0)
