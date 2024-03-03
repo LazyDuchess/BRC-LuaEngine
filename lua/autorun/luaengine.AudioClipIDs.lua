b1028371ad2db42f4c8610bd7fe485157a9c2e68
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-03-03 16:21:16 -0300

    [Lua] AudioClipIDs: Voices

---
+++ lua/autorun/luaengine.AudioClipIDs.lua
@@ -0,0 +1,11 @@
+--PRIORITY: -100
+
+AudioClipIDs = {}
+
+AudioClipIDs.VoiceDie = 484
+AudioClipIDs.VoiceDieFall = 485
+AudioClipIDs.VoiceTalk = 486
+AudioClipIDs.VoiceBoostTrick = 498
+AudioClipIDs.VoiceCombo = 487
+AudioClipIDs.VoiceGetHit = 488
+AudioClipIDs.VoiceJump = 489
