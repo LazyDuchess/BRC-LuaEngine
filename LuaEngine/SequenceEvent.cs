f4a383bfd17d278d856697349b50271e97929b67
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-03-04 00:48:00 -0300

    Fix skipping in sequenceevent

--- LuaEngine/SequenceEvent.cs
+++ LuaEngine/SequenceEvent.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine.Playables;
 
 namespace LuaEngine
 {
     public class SequenceEvent : GameplayEvent
     {
+        public PlayableDirector Director = null;
         public LuaEventHandler OnSequenceEnd = null;
         public override void Awake() {
             base.Awake();
@@ -18,7 +20,12 @@ namespace LuaEngine
         public override void LetPlayerGo()
         {
             base.LetPlayerGo();
+            Director.time = Director.duration;
+            Director.Evaluate();
+            if (WorldHandler.instance.CurrentCamera != WorldHandler.instance.GetCurrentPlayer().cam.cam)
+                    WorldHandler.instance.CurrentCamera.gameObject.SetActive(false);
             OnSequenceEnd?.Invoke();
+            Destroy(this.gameObject);
         }
     }
 }
