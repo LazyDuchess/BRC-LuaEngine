95ff829b6c6ffc0995d67ac5a316978e67175e55
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-03-04 02:05:21 -0300

    bundle all example maps in 1 package

--- LuaEngine.Editor/Assets/Scripts/Editor/DeveloperMenu.cs
+++ LuaEngine.Editor/Assets/Scripts/Editor/DeveloperMenu.cs
@@ -9,17 +9,10 @@ public static class DeveloperEditor
     [MenuItem("LuaEngine Developer/Export Example Maps")]
     private static void ExportExampleMaps()
     {
-        var outputPath = "Build";
+        var outputPath = "../Build";
         var mapsPath = "Assets/Maps";
-        var mapFolders = Directory.GetDirectories(mapsPath);
         if (!Directory.Exists(outputPath))
-        {
             Directory.CreateDirectory(outputPath);
-        }
-        foreach(var mapFolder in mapFolders)
-        {
-            var mapName = Path.GetFileName(mapFolder);
-            AssetDatabase.ExportPackage(new string[] { mapFolder }, Path.Combine(outputPath, mapName) + ".unitypackage", ExportPackageOptions.Recurse);
-        }
+        AssetDatabase.ExportPackage(new string[] { mapsPath }, Path.Combine(outputPath, "Example Maps.unitypackage"), ExportPackageOptions.Recurse);
     }
 }
