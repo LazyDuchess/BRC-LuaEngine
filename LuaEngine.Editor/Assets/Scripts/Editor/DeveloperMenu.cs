2f2ec10072f23f90a2dff5a2ae9066bfa3bbc5f0
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-03-04 23:15:52 -0300

    change name of example maps build

--- LuaEngine.Editor/Assets/Scripts/Editor/DeveloperMenu.cs
+++ LuaEngine.Editor/Assets/Scripts/Editor/DeveloperMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
+using LuaEngine.Editor;
 
 public static class DeveloperEditor
 {
@@ -13,6 +14,6 @@ public static class DeveloperEditor
         var mapsPath = "Assets/Maps";
         if (!Directory.Exists(outputPath))
             Directory.CreateDirectory(outputPath);
-        AssetDatabase.ExportPackage(new string[] { mapsPath }, Path.Combine(outputPath, "Example Maps.unitypackage"), ExportPackageOptions.Recurse);
+        AssetDatabase.ExportPackage(new string[] { mapsPath }, Path.Combine(outputPath, $"MapStation Example Maps {LuaEngineVersion.Version}.unitypackage"), ExportPackageOptions.Recurse);
     }
 }
