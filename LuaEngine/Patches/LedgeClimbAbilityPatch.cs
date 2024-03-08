f4c6f48d0d9acb453e3b51cbe3ceaab84b57281d
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-03-08 13:13:16 -0300

    Implement AllowClimb

---
+++ LuaEngine/Patches/LedgeClimbAbilityPatch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Reptile;
+
+namespace LuaEngine.Patches
+{
+    [HarmonyPatch(nameof(LedgeClimbAbility))]
+    internal static class LedgeClimbAbilityPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(LedgeClimbAbility.CheckActivation))]
+        private static bool CheckActivation_Prefix(ref bool __result, LedgeClimbAbility __instance)
+        {
+            var luaPlayer = PlayerLuaEngineComponent.Get(__instance.p);
+            if (luaPlayer == null) return true;
+            if (luaPlayer.AllowClimb) return true;
+            __result = false;
+            return false;
+        }
+    }
+}
